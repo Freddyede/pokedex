ebb5981107f69279a54f5a721dad7b947cc0e3d7
Franck <136079691+Freddyede@users.noreply.github.com>
2024-03-29 15:16:13 +0100

    Finish events.1

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,14 @@
 export const NavBar = ({ pokemonIndex, setPokemon, pokemons }) => (
     <>
         {
-            pokemons.map((pokemon, index) => (
-               <button key={ pokemon.name } onClick={ () => setPokemon(index) }>{ pokemon.name }</button>
-            ))
+            pokemons.map((pokemon, index) => {
+                if(pokemon.name === 'pikachu') {
+                    alert('pika pikachu !!!');
+                }
+                return (
+                    <button key={pokemon.name} onClick={() => setPokemon(index)}>{pokemon.name}</button>
+                )
+            })
         }
     </>
 );
